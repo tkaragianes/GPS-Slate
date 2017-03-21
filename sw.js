55b6ee2af731e7b68234792deb83fc729ad220c0
Thomas Karagianes <thomas@karagianes.com>
2017-03-21 12:39:20 -0700

    Initial Commit
    Initial commit of GPS-Slate project. Should contain working GPS nav
    feature and service worker to page for offline use.

---
+++ sw.js
@@ -0,0 +1,25 @@
+var CACHE_NAME = 'GPS-Slate-v1';
+var urlsToCache = [
+  '/GPS.html',
+  // '/styles/main.css',
+  // '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
